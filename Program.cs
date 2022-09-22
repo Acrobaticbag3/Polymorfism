688dd2b095823cb44c848c7e7409ab1204a6b9a0
Acrobaticbag3 <kevintom214@gmail.com>
2022-09-22 11:28:06 +0200

    fdfgfg

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@ using System;
 namespace Polymorfism {
     internal class Program {
         static void Main(string[] args) {
+            Shape shape;
             
+            (shape as Triangle).Area();
+            Triangle t = (Triangle)shape;
+            t = shape as Triangle;
         }
     }
 }
